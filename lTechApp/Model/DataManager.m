a8a8efa694784292e7dcc2ac55c1ecbd1ee9ecf1
Necrosoft <e.darnopyh@medsolutions.ru>
2016-10-27 19:35:29 +0300

    Поправил цикл.

--- lTechApp/Model/DataManager.m
+++ lTechApp/Model/DataManager.m
@@ -31,44 +31,25 @@
     modif = @[];
     new = @[];
     
-    /* Old */
-    
-  // if (self.storedItems.count > 0)
-  //  old = [self oldItemsWhichUpToDate:[self storedItems] andNewItems:[self itemsFromJSONResponse:data]];
-    
-   [self addAndUpdateItems:[self oldItemsWhichUpToDate:[self storedItems] andNewItems:[self itemsFromJSONResponse:data]] andNewItems:[self itemsFromJSONResponse:data]];
-    
-    /* Modified */
-    
-    /* New */
-    
-    [data enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+    if (self.storedItems.count > 0){
         
-        /* Mapping */
-        NSError *errorMdl;
-        ItemModel *mdl = [MTLJSONAdapter modelOfClass:[ItemModel class] fromJSONDictionary:obj error:&errorMdl];
+    /* Initial load (no saved data in db) */
         
-        /* Save JSON response to Core Data */
-        NSManagedObjectContext *context = [self managedObjectContext];
+        [self addAndUpdateItems:[self oldItemsWhichUpToDate:[self storedItems] andNewItems:[self itemsFromJSONResponse:data]] andNewItems:[self itemsFromJSONResponse:data]];
         
-        /* Create a new managed object */
-        NSManagedObject *item = [NSEntityDescription insertNewObjectForEntityForName:CD_ENTITY inManagedObjectContext:context];
-        [item setValue:mdl.itemImageLink.length > 0 ? mdl.itemImageLink : @"no_image" forKey:CD_IMAGE];
-        [item setValue:mdl.itemDate forKey:CD_DATE];
-        [item setValue:mdl.itemId forKey:CD_ID];
-        [item setValue:mdl.itemSort forKey:CD_SORT];
-        [item setValue:mdl.itemText.length > 0 ? mdl.itemText : @"no_text" forKey:CD_FULL_TEXT];
-        [item setValue:mdl.itemTitle.length > 0 ? mdl.itemTitle : @"no_title" forKey:CD_TITLE];
-
+    }   else {
         
-        NSError *error = nil;
-        // Save the object to persistent store
+        /* Data exist. Verify data for new, outdated and need to modfy items. */
         
-        if (![context save:&error]) {
-            NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
-        }
-        
-    }];
+        [data enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSError *error;
+            ItemModel *mdl = [MTLJSONAdapter modelOfClass:[ItemModel class] fromJSONDictionary:obj error:&error];
+            
+            /* Save JSON response to Core Data */
+            
+            [self addRecordWithItem:mdl];
+        }];
+    }
 }
 
 -(NSArray*)storedItems{
@@ -92,25 +73,27 @@
     return mutArr;
 }
 
-#pragma mark - comparsion
+#pragma mark - verification
 
-#pragma mark - new
-
--(NSArray*)addAndUpdateItems:(NSArray*)oldItems andNewItems:(NSArray*)newItems{
-    
-    NSMutableArray *mutArr = @[].mutableCopy;
+-(void)addAndUpdateItems:(NSArray*)oldItems andNewItems:(NSArray*)newItems{
     
     [newItems enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         
+     __block  BOOL isHaveNewObject = YES;
+
         [oldItems enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger oldIidx, BOOL * _Nonnull stop) {
             
             ItemModel *oldItem, *newItem;
             oldItem = oldItems[oldIidx];
             newItem = newItems[idx];
+
+          /*   Если объект из старого массива не равен объекту из нового массива он новый. Добавляем его, если он уже не был добавлен в результирующий массив */
             
-            /* Если объект из старого массива не равен объекту из нового массива он новый. Добавляем его, если он уже не был добавлен в результирующий массив */
             
             if ([oldItem isSameId:newItem]){
+                
+                NSLog(@"AAAAA");
+              isHaveNewObject = NO;
                 
                 /* Объект уже есть. Проверяем на необходимость обновления */
                 if (![oldItem isSameText:newItem]){
@@ -124,100 +107,171 @@
                     
                     NSError *error;
                     NSArray *results = [[self managedObjectContext] executeFetchRequest:request error:&error];
-                    NSLog(@"resultsPredic %@", results);
                   
                     /* Обновляем параметры */
                     if (results.count > 0)
                     [self updateObject:results[0] withItem:newItem];
     
                 }
-            }   else {
                 
-                /* Добавляем новый объект */
-                
-                if (![mutArr containsObject:newItem])
-                    [mutArr addObject:newItem];
-            }
-            
-            
+            }   else { }
+
         }];
+        
+        /* Объекта с таким ID не найдено. Добавляем новый объект. Проверяем не был ли он уже добавлен. */
+        NSLog(@"check book %i", isHaveNewObject);
+        if (isHaveNewObject)
+            [self addRecordWithItem:newItems[idx]];
+        
+        
     }];
     
-    return [NSArray arrayWithArray:mutArr];
+    
+//        for (int o = 0; o < oldItems.count; o ++) {
+//            
+//            for (int i = 0 ; i < newItems.count; i++) {
+//
+//            
+//            /* Если объект из старого массива не равен объекту из нового массива он новый. Добавляем его, если он уже не был добавлен в результирующий массив */
+//
+//            ItemModel *oldItem, *newItem;
+//            oldItem = oldItems[o];
+//            newItem = newItems[i];
+//
+//            if ([oldItem isSameId:newItem]){
+//                
+//                /* Объект уже есть. Проверяем на необходимость обновления */
+//                if (![oldItem isSameText:newItem]){
+//                    
+//                    /* Обновляем объект в базе */
+//                    /* Получаем объект из базы */
+//                    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+//                    [request setEntity:[NSEntityDescription entityForName:CD_ENTITY inManagedObjectContext:[self managedObjectContext]]];
+//                    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"itemId == %i", [newItem.itemId integerValue]];
+//                    [request setPredicate:predicate];
+//                    
+//                    NSError *error;
+//                    NSArray *results = [[self managedObjectContext] executeFetchRequest:request error:&error];
+//                    
+//                    /* Обновляем параметры */
+//                    if (results.count > 0)
+//                        [self updateObject:results[0] withItem:newItem];
+//                    
+//                }
+//            }   else {
+//                
+//                /* Объекта с таким ID не найдено. Добавляем новый объект */
+//                if (newItems.count == i)
+//                [self addRecordWithItem:newItem];
+//            }
+//            
+//        }
+//        
+//    }
 }
 
-
-
--(NSArray*)addedItems:(NSArray*)oldItems andNewItems:(NSArray*)newItems{
-    
-    NSMutableArray *mutArr = @[].mutableCopy;
-    
-    [newItems enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        
-        [oldItems enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger oldIidx, BOOL * _Nonnull stop) {
-            
-            ItemModel *oldItem, *newItem;
-            oldItem = oldItems[oldIidx];
-            newItem = newItems[idx];
-            
-            /* Если объект из старого массива не равен объекту из нового массива он новый. Добавляем его, если он уже не был добавлен в результирующий массив */
-            
-            if (![mutArr containsObject:newItem]){
-                
-            if (![oldItem isEqual:newItem])
-                [mutArr addObject:oldItem];
-            }
-        
-        }];
-    }];
-    
-    return [NSArray arrayWithArray:mutArr];
-}
 
 -(NSArray*)oldItemsWhichUpToDate:(NSArray*)oldItems andNewItems:(NSArray*)newItems{
     
     
     NSMutableArray *mutArr = @[].mutableCopy;
     
-    [newItems enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//    [newItems enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//        
+//        [oldItems enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger oldIidx, BOOL * _Nonnull stop) {
+//            
+//            ItemModel *oldItem, *newItem;
+//            oldItem = oldItems[oldIidx];
+//            newItem = newItems[idx];
+//            
+//            if ([oldItem isSameId:newItem]){
+//                /* Объект не устарел и присутствует в новой выборке */
+//                [mutArr addObject:oldItem];
+//
+//                
+//            }   else {
+//                
+//                /* Объект устарел, его нужно удалить */
+//                NSFetchRequest *request = [[NSFetchRequest alloc] init];
+//                [request setEntity:[NSEntityDescription entityForName:CD_ENTITY inManagedObjectContext:[self managedObjectContext]]];
+//                NSPredicate *predicate = [NSPredicate predicateWithFormat:@"itemId == %i", [newItem.itemId integerValue]];
+//                [request setPredicate:predicate];
+//                
+//                NSError *error;
+//                NSArray *results = [[self managedObjectContext] executeFetchRequest:request error:&error];
+//                if (!error && results.count > 0) {
+//                    for(NSManagedObject *managedObject in results){
+//                        [[self managedObjectContext] deleteObject:managedObject];
+//                    }
+//                    //Save context to write to store
+//                    [[self managedObjectContext] save:nil];
+//                }
+//            }
+//        }];
+//    }];
+    
+    
+    for (int i = 0; i < newItems.count; i++) {
         
-        [oldItems enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger oldIidx, BOOL * _Nonnull stop) {
-            
+        
+        for (int o = 0; o < oldItems.count ; o ++) {
+          
             ItemModel *oldItem, *newItem;
-            oldItem = oldItems[oldIidx];
-            newItem = newItems[idx];
+            oldItem = oldItems[o];
+            newItem = newItems[i];
             
             if ([oldItem isSameId:newItem]){
                 /* Объект не устарел и присутствует в новой выборке */
                 [mutArr addObject:oldItem];
-                
+                continue;
             }   else {
                 
-                /* Объект устарел, его нужно удалить */
-                NSFetchRequest *request = [[NSFetchRequest alloc] init];
-                [request setEntity:[NSEntityDescription entityForName:CD_ENTITY inManagedObjectContext:[self managedObjectContext]]];
-                NSPredicate *predicate = [NSPredicate predicateWithFormat:@"itemId == %i", [newItem.itemId integerValue]];
-                [request setPredicate:predicate];
-                
-                NSError *error;
-                NSArray *results = [[self managedObjectContext] executeFetchRequest:request error:&error];
-                if (!error && results.count > 0) {
-                    for(NSManagedObject *managedObject in results){
-                        [[self managedObjectContext] deleteObject:managedObject];
+                if (oldItems.count == o){
+                    
+                    /*  После прохождения всех элементов массива не было найдено совпадений объектов с таким ID, следовательно он отсутствует в новой выборке и его можно удалить. */
+                     
+                    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+                    [request setEntity:[NSEntityDescription entityForName:CD_ENTITY inManagedObjectContext:[self managedObjectContext]]];
+                    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"itemId == %i", [newItem.itemId integerValue]];
+                    [request setPredicate:predicate];
+                    
+                    NSError *error;
+                    NSArray *results = [[self managedObjectContext] executeFetchRequest:request error:&error];
+                    if (!error && results.count > 0) {
+                        for(NSManagedObject *managedObject in results){
+                            [[self managedObjectContext] deleteObject:managedObject];
+                        }
+                        //Save context to write to store
+                        [[self managedObjectContext] save:nil];
                     }
-                    //Save context to write to store
-                    [[self managedObjectContext] save:nil];
                 }
+                
             }
-            
-            
-            }];
-    }];
+        }
+    }
     
     return [NSArray arrayWithArray:mutArr];
 }
 
 #pragma mark - convinience
+
+-(void)addRecordWithItem:(ItemModel*)mdl{
+    
+    NSManagedObject *item = [NSEntityDescription insertNewObjectForEntityForName:CD_ENTITY inManagedObjectContext:[self managedObjectContext]];
+    [item setValue:mdl.itemImageLink.length > 0 ? mdl.itemImageLink : @"no_image" forKey:CD_IMAGE];
+    [item setValue:mdl.itemDate forKey:CD_DATE];
+    [item setValue:mdl.itemId forKey:CD_ID];
+    [item setValue:mdl.itemSort forKey:CD_SORT];
+    [item setValue:mdl.itemText.length > 0 ? mdl.itemText : @"no_text" forKey:CD_FULL_TEXT];
+    [item setValue:mdl.itemTitle.length > 0 ? mdl.itemTitle : @"no_title" forKey:CD_TITLE];
+    
+    /* Save the object to persistent store */
+    
+    NSError *error = nil;
+    if (![[self managedObjectContext] save:&error]) {
+        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
+    }
+}
 
 -(void)updateObject:(NSManagedObject*)managedObject withItem:(ItemModel*)item{
     
