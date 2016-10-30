eb472b7cdd7c36c6ba6721f7902e33a8f61f84ac
Necrosoft <e.darnopyh@medsolutions.ru>
2016-10-30 21:25:28 +0300

    Добавлены изображения.

---
+++ lTechApp/Base/Categories/NSString+WithoutScreening.h
@@ -0,0 +1,13 @@
+//
+//  NSString+WithoutScreening.h
+//  lTechApp
+//
+//  Created by Necrosoft on 30/10/2016.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (WithoutScreening)
+
+@end
---
+++ lTechApp/Base/Categories/NSString+WithoutScreening.m
@@ -0,0 +1,13 @@
+//
+//  NSString+WithoutScreening.m
+//  lTechApp
+//
+//  Created by Necrosoft on 30/10/2016.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+#import "NSString+WithoutScreening.h"
+
+@implementation NSString (WithoutScreening)
+
+@end
